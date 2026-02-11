e38ddc04498556cedbe5b3c56c81be99a6e30073
dbsebastian <sebastiann.db@gmail.com>
2026-02-11 18:01:09 -0300

    se actualiza lo hecho hasta final de clase 04

--- 1ra_clase.py
+++ 1ra_clase.py
@@ -6,14 +6,14 @@
 # imports
 
 import numpy as np
-import pandas
+#import pandas
 import matplotlib.pyplot as plt
 from scipy.stats import skew, kurtosis, chi2
 
 
 # inputs
 
-degrees_fr = 9
+degrees_fr = 2000
 size = 10**6
 
 distribucion_tipo = "chi" 
@@ -28,6 +28,8 @@ def estadisticos(var_x):
     
     mean_x = var_x.mean()
     
+    median_x = np.median(var_x)
+    
     std_x = var_x.std()
     
     kurt_x = kurtosis(var_x)
@@ -35,7 +37,7 @@ def estadisticos(var_x):
     skew_x = skew(var_x)
     
     
-    return mean_x, std_x, skew_x, kurt_x
+    return mean_x, median_x, std_x, skew_x, kurt_x
 
 
 
@@ -47,7 +49,7 @@ if distribucion_tipo == "normal":
     
     x = np.random.standard_normal(size)
     
-    mean_x, std_x, skew_x, kurt_x = estadisticos(x)
+    mean_x, median_x, std_x, skew_x, kurt_x = estadisticos(x)
     
     str_title = (f"Distribución tipo {distribucion_tipo}")
     
@@ -57,7 +59,7 @@ elif distribucion_tipo == "student":
 
     x = np.random.standard_t(df=degrees_fr, size=size)
     
-    mean_x, std_x, skew_x, kurt_x = estadisticos(x)
+    mean_x, median_x, std_x, skew_x, kurt_x = estadisticos(x)
     
     str_title = (f"Distribución tipo {distribucion_tipo} con {degrees_fr} grados de libertad")
     
@@ -67,7 +69,7 @@ elif distribucion_tipo == "exponencial":
     
     x = np.random.exponential(scale=degrees_fr, size=size )
     
-    mean_x, std_x, skew_x, kurt_x = estadisticos(x)
+    mean_x, median_x, std_x, skew_x, kurt_x = estadisticos(x)
     
     str_title = (f"Distribución tipo {distribucion_tipo} con coeficiente de {degrees_fr}")
     
@@ -77,7 +79,7 @@ elif distribucion_tipo == "chi":
     
     x = np.random.chisquare(df=degrees_fr, size=size )
     
-    mean_x, std_x, skew_x, kurt_x = estadisticos(x)
+    mean_x, median_x, std_x, skew_x, kurt_x = estadisticos(x)
     
     str_title = (f"Distribución tipo {distribucion_tipo}-cuadrado con {degrees_fr} grados de libertad")
     
@@ -86,7 +88,7 @@ elif distribucion_tipo == "chi":
 else:
     x = np.random.uniform(low=-1 , high=1, size=size)
     
-    mean_x, std_x, skew_x, kurt_x = estadisticos(x)
+    mean_x, median_x, std_x, skew_x, kurt_x = estadisticos(x)
 
     str_title = (f"Distribución tipo {distribucion_tipo}")
     
@@ -95,13 +97,44 @@ else:
 
 # title
 
-str_title += f'\n con media = {str(np.round(mean_x, 2))}'
+std_title = f' std = {str(np.round(std_x, 2))}'
 
-str_title += f'\n con std = {str(np.round(std_x, 2))}'
+str_title += f'\n con media = {str(np.round(mean_x, 2))}  ; {std_title}'
 
-str_title += f'\n con skew = {str(np.round(skew_x, 2))}'
+kurt_title = f' kurtosis = {str(np.round(kurt_x, 2))}'
 
-str_title += f'\n con kurtosis = {str(np.round(kurt_x, 2))}'
+str_title += f'\n skew = {str(np.round(skew_x, 2))}  ; {kurt_title}'
+
+
+
+
+
+
+
+# test de normalidad JB
+
+lado_izq = skew_x**2
+lado_der = (1/4) * ( kurt_x **2 )
+
+test_jb = (size/6) * (lado_der + lado_izq)  # acá tengo el valor JB
+
+# ahora al valor de JB debo obtener su CHI2
+chi_test = chi2.cdf(test_jb, df=2) 
+# función de probabilidad acumultativa a la izq del punto obtenido
+
+# ahora se obtiene el p_valor
+p_valor = 1 - chi_test
+
+es_normal = (p_valor > 0.05)
+
+if es_normal:
+    es_normal = "Normal"
+else:
+    es_normal = "no Normal"
+
+
+str_title += f"\n y con un P-Valor del Test JB de {np.round(p_valor, 4)}," 
+str_title += f"\n resultando en una distribución: {es_normal}"
 
 
 # plot
@@ -112,12 +145,8 @@ plt.title(str_title)
 
 plt.axvline(x=mean_x, label="mean", color="r")
 
+plt.axvline(x=median_x, label="median", color="b")
 
-# test
 
-lado_izq = skew_x**2
-lado_der = (1/4) * ( kurt_x **2 )
 
-test_jb = (size/6) * (lado_der + lado_izq) 
 
-print(f"El resultado del Test JB es {test_jb}")
